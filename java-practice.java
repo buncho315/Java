6e6dcff1860898565e3d7750a65161fcf1f3f902
buncho315 <mzh.hrtk125@gmail.com>
2022-10-01 15:10:32 +0000

    if

--- java-practice.java
+++ java-practice.java
@@ -17,3 +17,14 @@ public class Main {
 		System.out.println(a + b);
 	}
 }
+// if文による条件分岐
+public class Main {
+	public static void main(String[] args) {
+        int number = 3;
+        if(number == 1) {
+            System.out.println("suki!");
+        } else {
+            System.out.println("kirai!");
+        }
+	}
+}
