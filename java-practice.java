2a23ca3c15b67a0b9c614172b345a3382b42d8aa
buncho315 <mzh.hrtk125@gmail.com>
2022-10-01 14:54:21 +0000

    random計算問題

--- java-practice.java
+++ java-practice.java
@@ -1,5 +1,11 @@
 public class Main {
 	public static void main(String[] args) {
-		System.out.println((1234 + 5678) * 3);
+		int apple_price = (int)(Math.random()* 3 + 1)* 100; //リンゴの単価
+		int apple_num = (int)(Math.random() * 10 + 1); //リンゴを買う数
+		System.out.println("リンゴの単価:" + apple_price + "円");
+		System.out.println("リンゴを買う数" + apple_num + "個");
+
+		int total = apple_price * apple_num;
+		System.out.println("合計金額:" + total + "円");
 	}
 }
