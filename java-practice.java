eaada7f3f814d830f39e80b0f33e6df532521ab3
buncho315 <mzh.hrtk125@gmail.com>
2022-10-02 11:49:15 +0000

    条件分岐

--- java-practice.java
+++ java-practice.java
@@ -17,14 +17,17 @@ public class Main {
 		System.out.println(a + b);
 	}
 }
-// if文による条件分岐
+
+// if文による条件分岐　else if
 public class Main {
 	public static void main(String[] args) {
-        int number = 3;
-        if(number == 1) {
-            System.out.println("suki!");
-        } else {
-            System.out.println("kirai!");
-        }
+		int number = 1;
+		if (number == 1) {
+			System.out.println("スキ！");		// 条件式が成立したときの処理
+		} else if (number == 2) {
+		    System.out.println("どちらでもない");
+		} else {
+			System.out.println("キライ");		// 条件式が成立しなかったときの処理
+		}
 	}
 }
