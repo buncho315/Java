916f3df69031db9fb76c4cebc6bd355d46560337
buncho315 <mzh.hrtk125@gmail.com>
2022-10-04 11:50:06 +0000

    複数条件

--- java-practice.java
+++ java-practice.java
@@ -50,3 +50,26 @@ public class Main {
         System.out.println("平成" + heisei + "年です");
 	}
 }
+
+// 複数の条件を組み合わせる - AND
+
+public class Main {
+    public static void main(String[] args) {
+        int number1 = 2;
+        int number2 = 2;
+        if (number1 == 1 && number2 == 1) {
+            System.out.println("すき");
+        } else {
+            System.out.println("嫌い");
+        }
+
+        double rand = Math.random() * 100 + 1;
+        int number3 = (int)rand;
+        System.out.println(number3);
+        if (number3 >= 30 && number3 <= 60) {
+            System.out.println("あたり");
+        } else {
+            System.out.println("ハズレ");
+        }
+    }
+}
