92d0531df3abd81378e0b90faf1dd8b06d593def
buncho315 <mzh.hrtk125@gmail.com>
2022-10-07 05:42:24 +0000

    税込

--- java-practice.java
+++ java-practice.java
@@ -1,28 +1,20 @@
-public class Main {
-	public static void main(String[] args) {
-		int apple_price = (int)(Math.random()* 3 + 1)* 100; //リンゴの単価
-		int apple_num = (int)(Math.random() * 10 + 1); //リンゴを買う数
-		System.out.println("リンゴの単価:" + apple_price + "円");
-		System.out.println("リンゴを買う数" + apple_num + "個");
-
-		int total = apple_price * apple_num;
-		System.out.println("合計金額:" + total + "円");
-	}
-}
+// 税込み金額を計算する
 
 public class Main {
-	public static void main(String[] args) {
-		int time = 15;
-		if (time < 12) {
-			System.out.println("午前中"); // 条件式が成立したときの処理
-		} else if (time == 12) {
-		    System.out.println("正午");
-		} else if (time > 12) {
-		    System.out.println("午後");
-		}
-	}
+    public static void main(String[] args) {
+        int price = 3500;
+        System.out.println("定価"+"円");
+        int discountPrice =(int)(price * 0.85);
+        System.out.println("割引価格は"+discountPrice+"円です");
+        int amount =(int)(discountPrice * 1.08);
+        System.out.println("税込価格は"+amount+"円です");
+        int person = 5;
+        System.out.println("人数が"+person+"人の場合、");
+        int amountPerPerson = amount / person;
+        int remainder = amount % person;
+        System.out.println("一人当たりは"+amountPerPerson+"円、余りは"+remainder+"円です");
+    }
 }
-
 
 
 // if文による条件分岐　else if
