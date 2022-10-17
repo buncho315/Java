d50248f3b3fe6720cf4b66d839ecdee8ebb34b9d
buncho315 <mzh.hrtk125@gmail.com>
2022-10-17 12:51:36 +0000

    ループ処理

--- java-practice.java
+++ java-practice.java
@@ -188,3 +188,20 @@ public class Main {
         System.out.println("hello " + data);
     }
 }
+// 標準入力とループ処理
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int count = sc.nextInt();
+        System.out.println("データ個数： " + count);
+        
+        String data;
+        for (int i = 0; i < count; i++){
+            data = sc.next();
+            System.out.println("hello " + data);
+        }
+        
+    }
+}
